f303c862558eb468456af23aecf953ff86afac94
AIGyuri <gyorgyszabo0421@gmail.com>
2024-12-18 07:51:33 +0100

    241218csharp

---
+++ OrvosiNobeldijasok/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrvosiNobeldijasok
+{
+    class Elethossz
+    {
+        private int Tol { get; set; }
+        private int Ig { get; set; }
+        public int ElethosszEvekben => Tol == -1 || Ig == -1 ? -1 : Ig - Tol;
+
+        public bool IsmertAzElethossz => ElethosszEvekben != -1;
+
+        public Elethossz(string SzuletesHalalozas)
+        {
+            string[] m = SzuletesHalalozas.Split('-');
+            try
+            {
+                Tol = int.Parse(m[0]);
+            }
+            catch (Exception)
+            {
+                Tol = -1;
+            }
+            try
+            {
+                Ig = int.Parse(m[1]);
+            }
+            catch (Exception)
+            {
+                Ig = -1;
+            }
+        }
+    }
+    class Dijazott
+    {
+        public int Ev { get; private set; }
+        public string Nev { get; private set; }
+        public string SzuletesHalalozas { get; private set; }
+        public string Orszagkod { get; private set; }
+
+        public Dijazott(string sor)
+        {
+            string[] m = sor.Split(';');
+            Ev = int.Parse(m[0]);
+            Nev = m[1];
+            SzuletesHalalozas = m[2];
+            Orszagkod = m[3];
+        }
+
+        //2. Feladat.
+        //!!--nobeldíjak txt beolvasása--!!
+        public static List<Dijazott> LoadDB()
+        {
+            List<Dijazott> list = new List<Dijazott>();
+
+            StreamReader sr = new StreamReader("orvosi_nobeldijak.txt");
+
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                list.Add(new Dijazott(sr.ReadLine()));
+            }
+            sr.Close();
+
+            return list;
+        }
+
+    }
+
+    class OrvosiNobeldijasok
+    {
+
+
+        static void Main()
+        {
+            List<Dijazott> adatbazis = Dijazott.LoadDB();
+
+            
+
+
+            //3. Feladat.
+            Console.WriteLine($"3.Feladat: Díjazottak száma: {adatbazis.Count} fő");
+
+            //4. Feladat.
+
+            int utolsoEv = adatbazis.First().Ev;
+            foreach (var i in adatbazis.Skip(1)) 
+            {
+                if (i.Ev > utolsoEv) utolsoEv = i.Ev;
+            }
+            Console.WriteLine($"4. Feladat: Utolsó Év: {utolsoEv}");
+
+            //5. Feladat.
+            Console.WriteLine("5. Feladat: Kérem adja meg egy ország kódját: ");
+            string inputOrszagkod = Console.ReadLine().ToUpper();
+            List<Dijazott> inputOrszagDijazottjai = new List <Dijazott>();
+            foreach (var item in adatbazis)
+            {
+                if (item.Orszagkod == inputOrszagkod) inputOrszagDijazottjai.Add(item);
+            }
+            if (inputOrszagDijazottjai.Count == 0)
+            {
+                Console.WriteLine("\tA megadott országból nem volt díjazott!");
+            }
+            if (inputOrszagDijazottjai.Count == 1)
+            {
+                Console.WriteLine($"\tA megadott ország díjazottja:");
+                Console.WriteLine($"\tNév {inputOrszagDijazottjai.First().Nev}");
+                Console.WriteLine($"\tÉv {inputOrszagDijazottjai.First().Ev}");
+                Console.WriteLine($"\tSz/H {inputOrszagDijazottjai.First().SzuletesHalalozas}");
+            }
+            if (inputOrszagDijazottjai.Count > 1)
+            {
+                Console.WriteLine($"\tA megadott országból {inputOrszagDijazottjai.Count} fő díjazott volt!");
+            }
+
+
+            //6. Feladat
+            Console.WriteLine("6.Statisztika");
+            Dictionary<string, int> stat = new Dictionary<string, int>();
+            foreach (var i in adatbazis)
+            {
+                if (stat.ContainsKey(i.Orszagkod)) stat[i.Orszagkod]++;
+                else stat.Add(i.Orszagkod, 1);
+            }
+            foreach (var i in stat)
+            {
+                if (i.Value > 5) Console.WriteLine($"\t{i.Key} -  {i.Value} fő");
+            }
+                        Console.ReadKey();
+        }
+    }
+ }
---
+++ OrvosiNobeldijasokGUI/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace OrvosiNobeldijasokGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxNobelEv = new System.Windows.Forms.TextBox();
+            this.textBoxNobelNev = new System.Windows.Forms.TextBox();
+            this.textBoxNobelSzulHal = new System.Windows.Forms.TextBox();
+            this.textBoxNobelOrszag = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Év";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Név";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sz/H";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ország";
+            // 
+            // textBoxNobelEv
+            // 
+            this.textBoxNobelEv.Location = new System.Drawing.Point(138, 56);
+            this.textBoxNobelEv.Name = "textBoxNobelEv";
+            this.textBoxNobelEv.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNobelEv.TabIndex = 4;
+            // 
+            // textBoxNobelNev
+            // 
+            this.textBoxNobelNev.Location = new System.Drawing.Point(138, 102);
+            this.textBoxNobelNev.Name = "textBoxNobelNev";
+            this.textBoxNobelNev.Size = new System.Drawing.Size(212, 20);
+            this.textBoxNobelNev.TabIndex = 5;
+            // 
+            // textBoxNobelSzulHal
+            // 
+            this.textBoxNobelSzulHal.Location = new System.Drawing.Point(138, 147);
+            this.textBoxNobelSzulHal.Name = "textBoxNobelSzulHal";
+            this.textBoxNobelSzulHal.Size = new System.Drawing.Size(144, 20);
+            this.textBoxNobelSzulHal.TabIndex = 6;
+            // 
+            // textBoxNobelOrszag
+            // 
+            this.textBoxNobelOrszag.Location = new System.Drawing.Point(138, 188);
+            this.textBoxNobelOrszag.Name = "textBoxNobelOrszag";
+            this.textBoxNobelOrszag.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNobelOrszag.TabIndex = 7;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(169, 268);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(148, 30);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "Adatok mentése";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 329);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxNobelOrszag);
+            this.Controls.Add(this.textBoxNobelSzulHal);
+            this.Controls.Add(this.textBoxNobelNev);
+            this.Controls.Add(this.textBoxNobelEv);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Orvosi Nobel-díjasok";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxNobelEv;
+        private System.Windows.Forms.TextBox textBoxNobelNev;
+        private System.Windows.Forms.TextBox textBoxNobelSzulHal;
+        private System.Windows.Forms.TextBox textBoxNobelOrszag;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
